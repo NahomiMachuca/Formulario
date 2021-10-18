c76ac882e560e9757b8cd963eb5ce3ad496fae34
NahomiMachuca <89893604+NahomiMachuca@users.noreply.github.com>
2021-10-17 22:17:24 -0500

    Add files via upload

---
+++ Formularios/formulario.js
@@ -0,0 +1,70 @@
+
+//Para obtener los datos/ declaracion 
+const nombre = document.getElementById("myname");
+const apellidos = document.getElementById("surname");
+const correo = document.getElementById("email");
+const celular = document.getElementById("mobile");
+const contrasenia = document.getElementById("password");
+const contrasenia2 = document.getElementById("repeatPassword");
+const terminosYcondiciones = document.getElementById("termsAndConditions");
+const form = document.getElementById("form");
+const listInputs = document.querySelectorAll(".form-input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let condicion = validacionForm();
+  if (condicion) {
+    enviarFormulario();
+  }
+});
+
+function validacionForm() {
+  form.lastElementChild.innerHTML = "";
+  let condicion = true;
+  listInputs.forEach((element) => {
+    element.lastElementChild.innerHTML = "";
+  });
+  //validar el nombre si es que no ingresa ningun dato 
+  if (nombre.value.length < 1 || nombre.value.trim() == "") {
+    mostrarMensajeError("myname", "Nombre no valido*"); //para mostrar el mensaje de error
+    condicion = false;
+  }
+  //La misma validacion que en el nombre 
+  if (apellidos.value.length < 1 || apellidos.value.trim() == "") {
+    mostrarMensajeError("surname", "Apellido no valido");
+    condicion = false;
+  }
+  if (correo.value.length < 1 || correo.value.trim() == "") {
+    mostrarMensajeError("email", "Correo no valido*");
+    condicion = false;
+  }
+  //si el usuario ingresa un numero de telefono menor a 10 es un error
+  if (
+    celular.value.length != 10 ||
+    celular.value.trim() == "" ||
+    isNaN(celular.value)
+  ) {
+    mostrarMensajeError("mobile", "Celular no valido*");
+    condicion = false;
+  }
+  if (contrasenia.value.length < 1 || contrasenia.value.trim() == "") {
+    mostrarMensajeError("password", "Contraseña no valido*");
+    condicion = false;
+  }
+  if (contrasenia2.value != contrasenia.value) {
+    mostrarMensajeError("repeatPassword", "Contraseña error*");
+    condicion = false;
+  }
+  
+  return condicion;
+}
+
+function mostrarMensajeError(claseInput, mensaje) {
+  let elemento = document.querySelector(`.${claseInput}`);
+  elemento.lastElementChild.innerHTML = mensaje;
+}
+//una vez enviado el formulario se muestra 
+function enviarFormulario() {
+  form.reset();
+  form.lastElementChild.innerHTML = "Enviado con Exito!!";
+}
